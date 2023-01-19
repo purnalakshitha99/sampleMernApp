2cbd497d13ab38344a9e17070b450527963b439e
Purna Lakshitha <99383161+purnalakshitha99@users.noreply.github.com>
2023-01-19 14:55:34 +0530

    mongodb connection successfully

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+require("dotenv").config();
+
+mongoose.set("strictQuery", false);
+mongoose.set("strictQuery", true);
+
+const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("Mongodb Connection success!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running on port number: ${PORT}`);
+});
