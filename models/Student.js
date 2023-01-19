0946925473933478cfd4898779e7bd038e0b1e9e
Purna Lakshitha <99383161+purnalakshitha99@users.noreply.github.com>
2023-01-19 23:55:37 +0530

    Student module code is done

--- models/Student.js
+++ models/Student.js
@@ -11,4 +11,12 @@ const studentSchema = new Schema({
     type: Number,
     required: true,
   },
+  gender: {
+    type: String,
+    required: true,
+  },
 });
+
+const Student = mongoose.model("Student", studentSchema); //methanadi me student kiyanne document eka e kiyanne api udaharanayak lesa gattoth database eke thiyana table ekak thama student kiyanne //dan api student kiyana document ekata module eken gaththa values danawa  udaharanayak lessa gaththoth student kiyana table ekata temple ekak danwa wage wisthara danwa
+
+module.exports = Student;
